824ec9d1a96c782e49e9a2c982ab0f8b373cce0b
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-13 13:28:05 +0300

    backend - add user model

---
+++ backend/models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+      lowercase: true,
+    },
+    profile: {
+      type: String,
+      required: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    about: {
+      type: String,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    resetPasswordLink: {
+      data: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
