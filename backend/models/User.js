9fb469f89f8d8967bd9ca64ff0d285e5755a37a6
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-13 14:41:05 +0300

    backend - add auth routes and controllers with validations

--- backend/models/User.js
+++ backend/models/User.js
@@ -54,4 +54,44 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Apply Mongoose Virtuals for computed properties on documents
+
+UserSchema.virtual("password")
+  .set(function (password) {
+    // create a temporary variable _password
+    this.hashed_password = password;
+    // generate salt
+    this.salt = this.makeSalt();
+    // encrypt password
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this.hashed_password;
+  });
+
+UserSchema.methods = {
+  // get plain pass from client --> encrypt and checking against hashed pass in db
+  // if pass match --> authenticate user
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      console.error(err);
+      return "";
+    }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+
 module.exports = mongoose.model("User", UserSchema);
