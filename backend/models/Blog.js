2175a31b9024518a4d72becf49f3d557d2e2cc40
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-17 00:20:55 +0300

    backend - add blog model

---
+++ backend/models/Blog.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const BlogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      min: 3,
+      max: 160,
+      required: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      index: true,
+    },
+    body: {
+      type: {},
+      required: true,
+      min: 200,
+      max: 2000000,
+    },
+    excerpt: {
+      type: String,
+      max: 1000,
+    },
+    metatitle: {
+      type: String,
+    },
+    metadescription: {
+      type: String,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    // array of categories --> points to category model
+    categories: [{ type: ObjectId, ref: "Category", require: true }],
+    tags: [{ type: ObjectId, ref: "Tag", require: true }],
+    // reference blog author
+    postedBy: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Blog", BlogSchema);
