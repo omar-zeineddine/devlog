06d69eb5977808b6db8973b11eb764b038e5d0a9
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-16 15:31:15 +0300

    feat - add auth helper functions to store user information on client side

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -1,5 +1,6 @@
 import fetch from "isomorphic-fetch";
 import { API } from "../config";
+import cookie from "js-cookie";
 
 export const preSignup = (user) => {
   return fetch(`${API}/signup`, {
@@ -29,4 +30,73 @@ export const signin = (user) => {
       return res.json();
     })
     .catch((err) => console.error(err));
+};
+
+// storing user information before authentication
+
+// local storage
+
+export const setLocalStorage = (key, value) => {
+  if (typeof window !== "undefined") {
+    // run client side only
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
+export const removeLocalStorage = (key) => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem(key);
+  }
+};
+
+// cookie management
+
+export const getCookie = (key) => {
+  if (typeof window !== "undefined") {
+    return cookie.get(key);
+  }
+};
+
+export const setCookie = (key, value) => {
+  if (typeof window !== "undefined") {
+    cookie.set(key, value, {
+      expires: 1,
+    });
+  }
+};
+
+export const removeCookie = (key) => {
+  if (typeof window !== "undefined") {
+    cookie.remove(key, {
+      expires: 1,
+    });
+  }
+};
+
+// authenticate user by passing data to local storage and cookie
+export const authenticate = (data, next) => {
+  // save data to local storage
+  setLocalStorage("user", data.user);
+
+  // set the cookie
+  setCookie("token", data.token);
+
+  next();
+};
+
+// return authenticated user's information from local storage
+export const isAuthenticated = () => {
+  if (typeof window !== "undefined") {
+    // if there is a token in the cookie, then we have a user
+    const checkCookie = getCookie("token");
+
+    // if there is a cookie, get the 'user' item from the local storage
+    if (checkCookie) {
+      if (localStorage.getItem("user")) {
+        return JSON.parse(localStorage.getItem("user"));
+      } else {
+        return false;
+      }
+    }
+  }
 };
