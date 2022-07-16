1d793663be4bfe0e07d3d31e8f93bc6e4e1fbb4d
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-16 14:37:58 +0300

    feat - add signin component with backend linking

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -15,3 +15,18 @@ export const preSignup = (user) => {
     })
     .catch((err) => console.error(err));
 };
+
+export const signin = (user) => {
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.error(err));
+};
---
+++ frontend/components/auth/SigninComponent/SigninComponent.js
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+import { signin } from "../../../actions/auth";
+import Router from "next/router";
+
+const SigninComponent = () => {
+  const [values, setValues] = useState({
+    email: "user@user.com",
+    password: "123456",
+    error: "",
+    loading: false,
+    message: "",
+    showForm: true,
+  });
+
+  // destructure values from state
+  const { email, password, error, loading, message, showForm } = values;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("handle submit");
+    // console.table({ name, email, password, error, loading, message, showForm });
+    setValues({ ...values, loading: true, error: false });
+    const userData = { email, password };
+
+    signin(userData).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        // redirect to home page on successful login
+        Router.push("/");
+      }
+    });
+  };
+
+  const handleChange = (name) => (e) => {
+    // console.log(e.target.value);
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
+
+  // alerts
+  const showLoading = () =>
+    loading ? <div className="alert alert-info">Loading...</div> : "";
+
+  const showError = () =>
+    error ? <div className="alert alert-danger">{error}</div> : "";
+
+  const showMessage = () =>
+    message ? <div className="alert alert-info">{message}</div> : "";
+
+  const signinForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            value={email}
+            onChange={handleChange("email")}
+            type="email"
+            className="form-control"
+            placeholder="Enter your email"
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            value={password}
+            onChange={handleChange("password")}
+            type="password"
+            className="form-control"
+            placeholder="Enter your password"
+          />
+        </div>
+
+        <div>
+          <button className="btn btn-primary">login</button>
+        </div>
+      </form>
+    );
+  };
+
+  return (
+    <>
+      {showError()}
+      {showLoading()}
+      {showMessage()}
+      {showForm && signinForm()}
+    </>
+  );
+};
+
+export default SigninComponent;
