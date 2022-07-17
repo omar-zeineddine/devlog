3e5f5f479a6ea4db4c7d5e7796a927dff3234289
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-17 16:09:36 +0300

    feat - add blog form component and react qull rich text editor

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -9,7 +9,12 @@ export default function Document() {
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css"
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
         />
+        {/* import react-quill css */}
+        {/* https://www.npmjs.com/package/react-quill */}
+        <link rel="stylesheet" href="//cdn.quilljs.com/1.2.6/quill.snow.css" />
       </Head>
       <body>
         <Main />
