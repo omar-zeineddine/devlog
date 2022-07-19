1bdd0463815304e9f64a6515aca78172a04c50d3
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-19 21:49:53 +0300

    feat - display blog categories and tags on blogs page

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -3,8 +3,7 @@ import Layout from "../../components/Layout";
 import { useState } from "react";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import { API } from "../../config";
-import renderHTML from "react-render-html";
-import moment from "moment";
+import Card from "../../components/blog/Card";
 
 // grab properties that are returned from initial props
 const BlogsPage = ({ blogs, categories, tags, size }) => {
@@ -27,40 +26,7 @@ const BlogsPage = ({ blogs, categories, tags, size }) => {
                 blogs.map((blog, idx) => (
                   // output each blog as an article, having a unique key passed as idx
                   <article key={idx} className="mb-4">
-                    <div className="lead">
-                      <header>
-                        {/* use next link: clicking on blog title --> single blog view page */}
-                        <Link href={`/blogs/${blog.slug}`}>
-                          <a>
-                            <h2 className="py-3">{blog.title}</h2>
-                          </a>
-                        </Link>
-                      </header>
-                      <section>
-                        <p className="mark ml-1 py-2">
-                          Written by {blog.postedBy.name} | Published{" "}
-                          {moment(blog.updatedAt).fromNow()}
-                        </p>
-                      </section>
-                      <section>
-                        <p>show categories and tags</p>
-                      </section>
-
-                      <div className="row">
-                        <div className="col-xl-4">
-                          {/* todo: add image */}
-                          Image
-                        </div>
-                        <div className="col-xl-8">
-                          <section>
-                            <div>{renderHTML(blog.excerpt)}</div>
-                            <Link href={`/blog/${blog.slug}`}>
-                              <a className="btn btn-primary mt-3">Read More</a>
-                            </Link>
-                          </section>
-                        </div>
-                      </div>
-                    </div>
+                    <Card blog={blog} />
                     <hr />
                   </article>
                 ))}
