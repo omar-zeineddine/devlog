9708dc6b0f4d61d0338d56019e2bfe43f9e5732a
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-20 11:30:48 +0300

    feat - display content of single blog on frontend

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -12,7 +12,61 @@ const BlogPage = ({ blog, query }) => {
         <main>
           <article>
             <div className="container-fluid">
-              <section>{JSON.stringify(blog)}</section>
+              <section>
+                <div className="row">
+                  <img
+                    src={`${API}/blog/photo/${blog.slug}`}
+                    alt={blog.title}
+                    className="img img-fluid featured-image"
+                    style={{
+                      width: "100%",
+                      height: "60vh",
+                      objectFit: "cover",
+                    }}
+                  />
+                </div>
+              </section>
+            </div>
+
+            <div className="container">
+              <section>
+                <h1 className="py-3 text-center">{blog.title}</h1>
+                <p className="mark ml-1 py-2">
+                  Written by {blog.postedBy.name} | Published{" "}
+                  {moment(blog.updatedAt).fromNow()}
+                </p>
+              </section>
+              <section className="mb-3">
+                {blog.categories.map((category) => (
+                  <Link
+                    key={category._id}
+                    href={`/categories/${category.slug}`}
+                  >
+                    <a className="btn btn-primary mr-2 mt-1">{category.name}</a>
+                  </Link>
+                ))}
+
+                {blog.tags.map((tag) => (
+                  <Link key={tag._id} href={`/tags/${tag.slug}`}>
+                    <a className="btn btn-outline-primary mr-2 mt-1">
+                      {tag.name}
+                    </a>
+                  </Link>
+                ))}
+                <br />
+              </section>
+              <section>
+                <div className="row">
+                  <div className="col-xl-12">{renderHTML(blog.body)}</div>
+                </div>
+              </section>
+            </div>
+
+            <div className="container mt-5">
+              <h4 className="text-center">Related Blogs</h4>
+              <hr />
+              <p>todo: show related blogs</p>
+              <p>todo: show blog comments</p>
             </div>
           </article>
         </main>
