f6a6659917769202b162bfb67d357d835fe71f31
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-22 10:47:43 +0300

    backend - add route and controller to update user profile

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,8 @@
 const User = require("../models/User");
 const Blog = require("../models/Blog");
+const formidable = require("formidable");
+const slugify = require("slugify");
+const _ = require("lodash");
 const { errorHandler } = require("../utils/dbErrorHandler");
 
 exports.read = (req, res) => {
@@ -45,5 +48,60 @@ exports.publicProfile = (req, res) => {
           blogs: data,
         });
       });
+  });
+};
+
+// update user profile
+exports.updatePorfile = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "photo could not be uploaded",
+      });
+    }
+
+    let user = req.profile;
+    // merge changed fields into user
+    user = _.extend(user, fields);
+
+    // if there is any photo
+    if (files.photo) {
+      // prevent user from uploading photos bigger than 0.5 MB
+      if (files.photo.size > 500000) {
+        return res.status(400).json({
+          error: "Image should be less than 0.5MB in size",
+        });
+      }
+
+      blog.photo.data = fs.readFileSync(files.photo.filepath);
+      blog.photo.contentType = files.photo.mimetype;
+
+      user.save((err, result) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json(user);
+      });
+    }
+  });
+};
+
+exports.uploadProfilePhoto = (req, res) => {
+  const username = req.params.username;
+  User.findOne({ username }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found",
+      });
+    }
+    if (user.photo.data) {
+      res.set("Content-Type", user.photo.contentType);
+      return res.send(user.photo.data);
+    }
   });
 };
