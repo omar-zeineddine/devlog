fff5eaa1d6df0868e9b268b2c761838d2949aed0
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-22 10:11:47 +0300

    feat - display user info on profile page

--- frontend/pages/profile/[username].js
+++ frontend/pages/profile/[username].js
@@ -1,34 +1,57 @@
+import Link from "next/link";
 import Layout from "../../components/Layout";
 import moment from "moment";
 import { userProfile } from "../../actions/user";
 
-const UserProfile = ({ user, blogs }) => {
+const UserProfile = ({ user, blogs, query }) => {
   return (
-    <>
-      <Layout>
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-12">
-              <div className="card">
-                <div className="card-body">
-                  <h5>{user.name}</h5>
-                  <p className="text-muted">
-                    Joined {moment(user.createdAt).fromNow()}
-                  </p>
-                  {JSON.stringify(blogs)}
-                </div>
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-12">
+            <div className="card">
+              <div className="card-body">
+                <h5>{user.name}</h5>
+                <p className="text-muted">
+                  Joined {moment(user.createdAt).fromNow()}
+                </p>
               </div>
             </div>
           </div>
         </div>
-      </Layout>
-    </>
+      </div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <div className="p-3 mb-2 bg-info text-white">{`Latest blogs by ${user.name}`}</div>
+                {blogs.map((blog, idx) => (
+                  <div className="py-2" key={idx}>
+                    <Link href={`/blogs/${blog.slug}`}>
+                      <a className="lead">{blog.title}</a>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <div className="p-3 mb-2 bg-info text-white">{`Message ${user.name}`}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
-UserProfile.getInitialProps = async ({ query: { username } }) => {
-  const response = await userProfile(username);
-  return { user: response.user, blogs: response.blogs };
+UserProfile.getInitialProps = async ({ query }) => {
+  const response = await userProfile(query.username);
+  return { user: response.user, blogs: response.blogs, query };
 };
 
 export default UserProfile;
