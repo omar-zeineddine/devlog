07d12a34d72dc1b9ff9c8ef129a4f7dc0553c207
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-25 16:54:53 +0300

    backend - complete email contact api and sendgrid single sender verification

--- backend/controllers/form.js
+++ backend/controllers/form.js
@@ -1,3 +1,33 @@
+const sendgridMail = require("@sendgrid/mail");
+sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 exports.contactForm = (req, res) => {
-  res.send("contact endpoint");
+  //   res.send("contact endpoint");
+  const { email, name, message } = req.body;
+  // console.log(req.body);
+
+  const emailData = {
+    to: process.env.EMAIL_TO,
+    from: req.body.email,
+    subject: `Contact form - devlog`,
+    text: `Email received from 
+        Sender name: ${name}
+        Sender email: ${email}
+        Sender message: ${message}
+    `,
+    // html email on supported clients
+    html: `
+        <h3>Email received from ${email}</h3>
+        <h3>Name: ${name}</h3>
+        <h3>Email: ${email}</h3>
+        <p>Message:  ${message}</p>
+        <hr/>
+        <p>Email may contain sensitive information</p>
+    `,
+  };
+  sendgridMail.send(emailData).then((sent) => {
+    return res.json({
+      success: true,
+    });
+  });
 };
