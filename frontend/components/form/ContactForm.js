5a9a2f83ea94a66b0a6615a16dc13c59baaa50a8
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-25 17:26:22 +0300

    feat - initialize contact page and contact form component

---
+++ frontend/components/form/ContactForm.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { emailContactForm } from "../../actions/form";
+
+const ContactForm = () => {
+  return (
+    <>
+      <p>show contact form</p>
+    </>
+  );
+};
+
+export default ContactForm;
