b2f6c3b29ad8f8951e30adfcca232bfbb8295f24
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-27 11:18:47 +0300

    feat - update index page adding a category card

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,19 +1,58 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 
-const Index = () => {
-  return (
-    <Layout>
-      {/* <h2>Index Page</h2>
-      <Link href="/signup">
-        <a>signup</a>
-      </Link>
-      <br />
-      <Link href="/signin">
-        <a>signin</a>
-      </Link> */}
-    </Layout>
-  );
-};
+const Index = () => (
+  <Layout>
+    <article className="overflow-hidden py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center py-5">
+            <h1 className="display-5 font-weight-bold">
+              webdev and cloud blogs
+            </h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center pt-4 pb-5">
+            <p className="lead">
+              practical web development and cloud related blogs
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="flip">
+              <div
+                className="front"
+                style={{
+                  backgroundImage:
+                    "url(" + "https://i.imgur.com/u6YtqLj.gif" + ")",
+                }}
+              >
+                <h3 className="text-center h1">MERN</h3>
+              </div>
+              <div className="back text-center">
+                <Link href="/categories/mern">
+                  <a>
+                    <h4 className="h1">MERN</h4>
+                  </a>
+                </Link>
+                <p className="lead">
+                  A free and open-source JavaScript software stack for building
+                  dynamic web sites and web applications.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  </Layout>
+);
 
 export default Index;
