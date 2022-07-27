16cd3c1af5c5974f858262e699d710982e2ec8f8
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-27 21:47:43 +0300

    feat - add website footer

---
+++ frontend/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="container">
+      <hr />
+      <p className="text-center font-weight-bold">
+        Devlog© - {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
