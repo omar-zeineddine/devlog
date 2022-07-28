24128f76009fdebbf2183554f05740231156a65e
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-28 15:57:54 +0300

    refactor - import a seprate user dashboard component

--- frontend/pages/user/index.js
+++ frontend/pages/user/index.js
@@ -1,39 +1,12 @@
 import Link from "next/link";
 import Layout from "../../components/Layout";
 import Private from "../../components/auth/Private/Private";
+import UserDashboard from "../../components/UserDashboard/UserDashboard";
 
 const UserPage = () => (
   <Layout>
     <Private>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 py-5">
-            <h4>User Dashboard</h4>
-          </div>
-          <div className="col-md-4">
-            <ul className="list-group">
-              <li className="list-group-item">
-                <Link href="/user/crud/blog">
-                  <a>Create Blog</a>
-                </Link>
-              </li>
-
-              <li className="list-group-item">
-                <Link href="/user/crud/blogs">
-                  <a>Update/Delete Blogs</a>
-                </Link>
-              </li>
-
-              <li className="list-group-item">
-                <Link href="/user/update">
-                  <a>Update Profile</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="col-md-8"></div>
-        </div>
-      </div>
+      <UserDashboard />
     </Private>
   </Layout>
 );
