bc4240857c197957286ddf56e48775ec588671bb
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-28 14:14:03 +0300

    refactor - update page alignment

--- frontend/pages/profile/[username].js
+++ frontend/pages/profile/[username].js
@@ -7,51 +7,46 @@ import ContactForm from "../../components/form/ContactForm";
 const UserProfile = ({ user, blogs, query }) => {
   return (
     <Layout>
-      <div className="container">
-        <div className="row">
-          <div className="col-xl-12">
-            <div className="card">
-              <div className="card-body p-1">
-                <h5>{user.name}</h5>
-                <p className="text-muted">
-                  Joined {moment(user.createdAt).fromNow()}
-                </p>
-              </div>
+      <div className="row">
+        <div className="col-xl-12">
+          <div className="card">
+            <div className="card-body p-1">
+              <h5>{user.name}</h5>
+              <p className="text-muted">
+                Joined {moment(user.createdAt).fromNow()}
+              </p>
             </div>
           </div>
         </div>
       </div>
-      <div className="container mt-2">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="card">
-              <div className="card-body">
-                <div className="p-3 mb-2 bg-main text-white">{`Latest blogs by ${user.name}`}</div>
-                {blogs.map((blog, idx) => (
-                  <div className="py-2" key={idx}>
-                    <Link href={`/blogs/${blog.slug}`}>
-                      <a className="lead">{blog.title}</a>
-                    </Link>
 
-                    <p className="profile__blog-card__posted">
-                      Posted at -
-                      <span className="profile__blog-card__posted-date">
-                        {moment(blog.createdAt).format(
-                          "MMMM Do YYYY, h:mm:ss a"
-                        )}
-                      </span>
-                    </p>
-                  </div>
-                ))}
-              </div>
+      <div className="row my-4">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <div className="p-3 mb-2 bg-main text-white">{`Latest blogs by ${user.name}`}</div>
+              {blogs.map((blog, idx) => (
+                <div className="py-2" key={idx}>
+                  <Link href={`/blogs/${blog.slug}`}>
+                    <a className="lead">{blog.title}</a>
+                  </Link>
+
+                  <p className="profile__blog-card__posted">
+                    Posted at -
+                    <span className="profile__blog-card__posted-date">
+                      {moment(blog.createdAt).format("MMMM Do YYYY, h:mm:ss a")}
+                    </span>
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
-          <div className="col-md-6">
-            <div className="card mb-5">
-              <div className="card-body">
-                <div className="p-3 mb-2 bg-main text-white">{`Message ${user.name}`}</div>
-                <ContactForm authorEmail={user.email} />
-              </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card mb-5">
+            <div className="card-body">
+              <div className="p-3 mb-2 bg-main text-white">{`Message ${user.name}`}</div>
+              <ContactForm authorEmail={user.email} />
             </div>
           </div>
         </div>
