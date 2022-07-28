4815068db163a6e9dc2c31bbe719cbc025fac771
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-28 15:51:03 +0300

    feat - finalize admin dashboard

--- frontend/components/AdminDashboard/AdminDashboard.js
+++ frontend/components/AdminDashboard/AdminDashboard.js
@@ -1,54 +1,78 @@
 import Link from "next/link";
-// import Categories from "../crud/Categories/Categories";
-// import CreateBlog from "../crud/CreateBlog/CreateBlog";
 import styles from "./AdminDashboard.module.scss";
 
 const AdminDashboard = () => {
   return (
-    <section className={`${styles.adminDashboard} `}>
-      <div>
-        <h4 className={`${styles.adminDashboard__Title} py-2 mb-2`}>
-          Admin Dashboard
-        </h4>
-      </div>
+    <div className="container">
+      <h4 className={`${styles.adminDashboard__Title} py-2 mb-2`}>
+        Admin Dashboard
+      </h4>
       <div className="row">
-        <ul className={styles.adminDashboard__listGroup}>
-          {/* Categories */}
-          <li className={styles.adminDashboard__listGroupItem}>
-            <img
-              className={styles.adminDashboard__listGroupItem__icon}
-              src="assets/icons/categories.png"
-              alt=""
-            />
-            <Link href="/admin/crud/category">
-              <a>Manage Categories</a>
-            </Link>
-          </li>
-          {/* Tags*/}
-          <li className={styles.adminDashboard__listGroupItem}>
-            <img
-              className={styles.adminDashboard__listGroupItem__icon}
-              src="assets/icons/tags.png"
-              alt=""
-            />
-            <Link href="/admin/crud/tag">
-              <a>Manage Tags</a>
-            </Link>
-          </li>
-          {/* create blog */}
-          <li className={styles.adminDashboard__listGroupItem}>
-            <img
-              className={styles.adminDashboard__listGroupItem__icon}
-              src="assets/icons/create.png"
-              alt=""
-            />
-            <Link href="/admin/crud/blog">
-              <a>Create Blog</a>
-            </Link>
-          </li>
-        </ul>
+        <div className="col-md-6">
+          <ul className={styles.adminDashboard__listGroup}>
+            {/* Categories */}
+            <li className={styles.adminDashboard__listGroupItem}>
+              <img
+                className={styles.adminDashboard__listGroupItem__icon}
+                src="assets/icons/categories.png"
+                alt=""
+              />
+              <Link href="/admin/crud/category">
+                <a>Manage Categories</a>
+              </Link>
+            </li>
+            {/* Tags*/}
+            <li className={styles.adminDashboard__listGroupItem}>
+              <img
+                className={styles.adminDashboard__listGroupItem__icon}
+                src="assets/icons/tags.png"
+                alt=""
+              />
+              <Link href="/admin/crud/tag">
+                <a>Manage Tags</a>
+              </Link>
+            </li>
+            {/* create blog */}
+            <li className={styles.adminDashboard__listGroupItem}>
+              <img
+                className={styles.adminDashboard__listGroupItem__icon}
+                src="assets/icons/create.png"
+                alt=""
+              />
+              <Link href="/admin/crud/blog">
+                <a>Create Blog</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-6">
+          <ul className={styles.adminDashboard__listGroup}>
+            {/* Blogs */}
+            <li className={styles.adminDashboard__listGroupItem}>
+              <img
+                className={styles.adminDashboard__listGroupItem__icon}
+                src="assets/icons/update-delete.png"
+                alt=""
+              />
+              <Link href="/admin/crud/blogs">
+                <a>Update / Delete Blogs</a>
+              </Link>
+            </li>
+            {/* Profile */}
+            <li className={styles.adminDashboard__listGroupItem}>
+              <img
+                className={styles.adminDashboard__listGroupItem__icon}
+                src="assets/icons/profile.png"
+                alt=""
+              />
+              <Link href="/user/update">
+                <a>Update Profile</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
