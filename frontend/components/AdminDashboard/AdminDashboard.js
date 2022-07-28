3ccd133186b5102ce8765d0e08dc0c17216d3017
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-07-28 15:29:00 +0300

    refactor - update admin dashboard content

--- frontend/components/AdminDashboard/AdminDashboard.js
+++ frontend/components/AdminDashboard/AdminDashboard.js
@@ -1,16 +1,52 @@
 import Link from "next/link";
-import Categories from "../crud/Categories/Categories";
+// import Categories from "../crud/Categories/Categories";
+// import CreateBlog from "../crud/CreateBlog/CreateBlog";
+import styles from "./AdminDashboard.module.scss";
 
 const AdminDashboard = () => {
   return (
-    <section className="container-fluid">
+    <section className={`${styles.adminDashboard} `}>
+      <div>
+        <h4 className={`${styles.adminDashboard__Title} py-2 mb-2`}>
+          Admin Dashboard
+        </h4>
+      </div>
       <div className="row">
-        <div className="col-md-12 pt-5 pb-5">
-          <h4>Admin Dashboard</h4>
-        </div>
-        <div className="col-md-6">
-          <Categories />
-        </div>
+        <ul className={styles.adminDashboard__listGroup}>
+          {/* Categories */}
+          <li className={styles.adminDashboard__listGroupItem}>
+            <img
+              className={styles.adminDashboard__listGroupItem__icon}
+              src="assets/icons/categories.png"
+              alt=""
+            />
+            <Link href="/admin/crud/category">
+              <a>Manage Categories</a>
+            </Link>
+          </li>
+          {/* Tags*/}
+          <li className={styles.adminDashboard__listGroupItem}>
+            <img
+              className={styles.adminDashboard__listGroupItem__icon}
+              src="assets/icons/tags.png"
+              alt=""
+            />
+            <Link href="/admin/crud/tag">
+              <a>Manage Tags</a>
+            </Link>
+          </li>
+          {/* create blog */}
+          <li className={styles.adminDashboard__listGroupItem}>
+            <img
+              className={styles.adminDashboard__listGroupItem__icon}
+              src="assets/icons/create.png"
+              alt=""
+            />
+            <Link href="/admin/crud/blog">
+              <a>Create Blog</a>
+            </Link>
+          </li>
+        </ul>
       </div>
     </section>
   );
