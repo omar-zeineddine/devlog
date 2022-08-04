88c6d30d2496d1af1cb7221004d496ab6bcc12d8
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-08-04 20:34:36 +0300

    backend - finaliza backend commits

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const Blog = require("../models/Blog");
 const formidable = require("formidable");
 const _ = require("lodash");
-const slugify = require("slugify");
+const fs = require("fs");
 const { errorHandler } = require("../utils/dbErrorHandler");
 
 exports.read = (req, res) => {
