a306fe3611549db31d6fd5e6f13ad5bac2b9e0f6
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-08-06 12:08:51 +0300

    fix - add head meta tag to _document page

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,6 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <body>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         <Main />
         <NextScript />
       </body>
